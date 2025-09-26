d84e79330c15ba1e6530a4a68c410a3f39b9972c
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-26 16:32:31 +0200

    fichier moduleconnexion.sql réalisé

--- moduleconnexion.sql
+++ moduleconnexion.sql
@@ -1,7 +1,7 @@
 -- Active: 1753689472411@@127.0.0.1@3306@moduleconnexion
 
 Use moduleconnexion;
-
+-- Table utilisateurs :
 CREATE TABLE IF NOT EXISTS utilisateurs (
   id INT AUTO_INCREMENT PRIMARY KEY,
   login VARCHAR(255) NOT NULL UNIQUE,
