e5a645456b6844187da52ea2e07dd9afdd1aff30
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-09-26 14:43:12 +0200

    personnalisation du fichier index.php

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php include '../includes/header.php'; ?>
+
+<h1 class="text-3xl font-bold mb-4">Bienvenue sur ModuleConnexion</h1>
+<p class="text-lg text-gray-700 mb-6">
+  Ce site vous permet de créer un compte, de vous connecter et de gérer vos informations.
+</p>
+
+<div class="flex space-x-4">
+  <a href="inscription.php" class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700">
+    S’inscrire
+  </a>
+  <a href="connexion.php" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+    Se connecter
+  </a>
+</div>
+
+<?php include '../includes/footer.php'; ?>
